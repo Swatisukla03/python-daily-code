2a273b9fe881248f389278aae1a29f3e3a544df9
swatisukla03 <swatisukla16@gmail.com>
2021-10-08 07:22:22 +0530

    string

---
+++ string college.py
@@ -0,0 +1,44 @@
+#string in python
+#searching a word in string using loop
+'''txt="The best thing in life are free!"
+if "mild" in txt:
+ print("yes free is present in text.")
+else:
+ print("Not Present")
+ #slicing
+ b="hello Swati Sukla"
+ print(b[1:])
+ print(b[-2:])
+ print(b[-4:])
+ print(b[ :6])
+ #slicing from the start
+ b="Hello World"
+ print(b[:6])
+ print(b[:13])
+ # upper case
+ b="Hello,World"
+ print(b[1:13:3])
+ #Remove whitespace from the beginning
+ a="Hello World"
+ print(a.strip())'''
+#Replace the string
+a="Hello world"
+print(a.replace("H","J"))
+#Split string
+a= "Hello, World"
+print(a.split(","))
+#string concatenation
+a="Hello"
+b=" Good Morning Swati"
+c=a+b
+print(c)
+print(a+b)
+#string format
+age=23
+name="Swati Sukla"
+txt="Hey this is {} , i am {}"
+print(txt.format(name,age))
+print(txt.format(name,age))
+
+
+
